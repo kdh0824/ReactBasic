5dd005cef54bbb19361a278530e13e3e5bc0da1e
KDH <rkdehdgus57@gmail.com>
2021-10-30 21:11:33 +0900

    UseRef

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import UserList from "./UserList";
 
 function App() {
+    const users = [
+        {
+            id: 1,
+            username: 'kdh',
+            email: 'public.kdh@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com  '
+        },
+        {
+            id: 3,
+            username: 'kdh00',
+            email: 'kdh00@example.com'
+        }
+    ];
+
+    const nextId = useRef(4);
+
+    const onCreate = () => {
+        console.log(nextId.current);
+        nextId.current += 1;
+    }
+
     return (
-        <UserList />
+        <UserList users={users}/>
     );
 }
 
